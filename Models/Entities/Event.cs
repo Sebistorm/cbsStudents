4a9c35bc525523953d6249002779a0d1f9329bcc
LAPTOP-4141SHPJ\seb99 <seb994@live.dk>
2022-05-27 12:14:54 +0200

    Add project files.

---
+++ Models/Entities/Event.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace cbsStudents.Models.Entities
+
+{
+    public class Event
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please fill out title")]
+        [MinLength(3, ErrorMessage = "Minimum 3 characters")]
+        public string Title { get; set; }
+
+        [Required]
+        public DateOnly StartDate { get; set; }
+
+        [Required]
+        public DateOnly EndDate { get; set; }
+
+        [Required]
+        public TimeOnly StartTime { get; set; }
+
+        [Required]
+        public TimeOnly EndTime { get; set; }
+
+        [Required]
+        [MinLength(10)]
+        public string Description { get; set; }
+
+
+    }
+}
+
